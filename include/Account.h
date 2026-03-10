23d276039b9c0e3f2d02b310350e182f6c404d47
paulinazh786 <paulinazhukunova@gmail.com>
2026-03-11 00:20:37 +0300

    structure created

---
+++ include/Account.h
@@ -0,0 +1,8 @@
+//
+// Created by пк on 11.03.2026.
+//
+
+#ifndef BANKSYSTEM_ACCOUNT_H
+#define BANKSYSTEM_ACCOUNT_H
+
+#endif //BANKSYSTEM_ACCOUNT_H
---
+++ include/Bank.h
@@ -0,0 +1,8 @@
+//
+// Created by пк on 11.03.2026.
+//
+
+#ifndef BANKSYSTEM_BANK_H
+#define BANKSYSTEM_BANK_H
+
+#endif //BANKSYSTEM_BANK_H
