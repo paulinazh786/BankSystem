1bc3ffd44a0dbf43ffb4406a950195ce43f9d794
paulinazh786 <paulinazhukunova@gmail.com>
2026-03-11 22:45:47 +0300

    account.h update

--- include/Account.h
+++ include/Account.h
@@ -1,8 +1,47 @@
-//
-// Created by пк on 11.03.2026.
-//
+#pragma once
 
-#ifndef BANKSYSTEM_ACCOUNT_H
-#define BANKSYSTEM_ACCOUNT_H
+#include "Transaction.h"
+#include <string>
+#include <vector>
+#include <memory>
+#include <chrono>
+#include <optional>
 
-#endif //BANKSYSTEM_ACCOUNT_H
+class Client;
+
+class Account : public std::enable_shared_from_this<Account> {
+protected:
+    std::string accountNumber;
+    double balance;
+    std::weak_ptr<Client> owner;
+    std::chrono::system_clock::time_point openDate;
+    bool isActive;
+    std::vector<Transaction> transactions;
+
+protected:
+    double& getBalanceRef() { return balance; }
+
+    +++++++++
+
+public:
+    Account(const std::string& accountNumber,
+            std::shared_ptr<Client> owner,
+            double initialBalance = 0.0);
+
+    virtual ~Account() = default;
+
+    std::string getAccountNumber() const { return accountNumber; }
+
+    double getBalance() const { return balance; }
+
+    std::shared_ptr<Client> getOwner() const;
+
+    std::chrono::system_clock::time_point getOpenDate() const { return openDate; }
+
+    bool getIsActive() const { return isActive; }
+
+    const std::vector<Transaction>& getTransactionHistory() const { return transactions; }
+
+    std::vector<Transaction> getTransactionsByDate(
+        const std::chrono::system_clock::time_point& startDate,
+        const std::chrono::system_clock::time_point& endDate) const;
