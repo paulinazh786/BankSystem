cf8ab9f65159093cd495bfb5374a42d781416fe6
llestiaq <m39764245@gmail.com>
2026-03-12 17:11:20 +0300

    depositaccount header getters

--- include/DepositAccount.h
+++ include/DepositAccount.h
@@ -1,4 +1,44 @@
-#ifndef BANKSYSTEM_DEPOSITACCOUNT_H
-#define BANKSYSTEM_DEPOSITACCOUNT_H
+#pragma once
 
-#endif //BANKSYSTEM_DEPOSITACCOUNT_H
+#include "Account.h"
+#include <string>
+#include <memory>
+#include <chrono>
+
+class DepositAccount : public Account {
+public:
+
+    enum class PaymentSchedule {
+        AtEnd,
+        Monthly
+    };
+
+private:
+    double percent;
+    bool isRefillable;
+    std::chrono::system_clock::time_point endDate;
+    PaymentSchedule paymentSchedule;
+    bool isCapitalization;
+
+    DepositAccount(const std::string& accountNumber,
+                      std::shared_ptr<Client> owner,
+                      double percent,
+                      const std::chrono::system_clock::time_point& endDate,
+                      bool isRefillable = false,
+                      PaymentSchedule paymentSchedule = PaymentSchedule::AtEnd,
+                      bool isCapitalization = false,
+                      double initialBalance = 0.0);
+
+
+    double getPercent() const { return percent; }
+
+    bool getIsRefillable() const { return isRefillable; }
+
+    std::chrono::system_clock::time_point getEndDate() const { return endDate; }
+
+    int getDaysUntilMaturity() const;
+
+    PaymentSchedule getPaymentSchedule() const { return paymentSchedule; }
+
+    bool getIsCapitalization() const { return isCapitalization; }
+
